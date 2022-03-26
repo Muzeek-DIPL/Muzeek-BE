dcc05afcbc7fd0ea5d4bb60f5c42134c8f2b11d3
iskandardevan <devvv13@gmail.com>
2022-03-26 19:53:03 +0700

    migrating

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username', 50);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password', 50);
+            $table->string('full_name', 50);
+            $table->text('about');
+            $table->string('location', 50);
+            $table->string('phone', 16);
+            $table->text('img_link');
             $table->rememberToken();
             $table->timestamps();
         });
